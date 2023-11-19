998b1f77e3a264408845f6a7e88ef13afc47efbb
HenryMorgan2 <taraskovdanil@gmail.com>
2023-11-19 16:32:25 +0300

    first commit

---
+++ src/main/java/com/github/henrymorgan2/paymentschedulecalculator/PaymentScheduleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.henrymorgan2.paymentschedulecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentScheduleCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentScheduleCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/henrymorgan2/paymentschedulecalculator/PaymentScheduleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.henrymorgan2.paymentschedulecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentScheduleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
